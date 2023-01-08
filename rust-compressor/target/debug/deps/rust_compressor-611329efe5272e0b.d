814c81ddba6fdcd9446b7b3e86340b4a8513829a
Cameron Richards <crichards1717@gmail.com>
2023-01-08 12:09:13 -0800

    Initialized

---
+++ rust-compressor/target/debug/deps/rust_compressor-611329efe5272e0b.d
@@ -0,0 +1,5 @@
+c:\Users\crich\Documents\Coding\Projects\UUID Comp\Rust_Compressor\rust-compressor\target\debug\deps\rust_compressor-611329efe5272e0b.rmeta: src\main.rs
+
+c:\Users\crich\Documents\Coding\Projects\UUID Comp\Rust_Compressor\rust-compressor\target\debug\deps\rust_compressor-611329efe5272e0b.d: src\main.rs
+
+src\main.rs:
---
+++ rust-compressor/target/debug/deps/rust_compressor-87776baf295383c9.d
@@ -0,0 +1,5 @@
+c:\Users\crich\Documents\Coding\Projects\UUID Comp\Rust_Compressor\rust-compressor\target\debug\deps\rust_compressor-87776baf295383c9.rmeta: src\main.rs
+
+c:\Users\crich\Documents\Coding\Projects\UUID Comp\Rust_Compressor\rust-compressor\target\debug\deps\rust_compressor-87776baf295383c9.d: src\main.rs
+
+src\main.rs:
