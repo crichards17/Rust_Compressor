ac7bf55ae7fcfaf159efc374f6286317a6d11cd6
Cameron Richards <crichards1717@gmail.com>
2023-02-08 15:02:59 -0800

    to_uuid() and to_uuid_string() for StableId. Reimplemented arithmetic ops for StableId (now safe).

--- id-compressor/src/id_types/session_id.rs
+++ id-compressor/src/id_types/session_id.rs
@@ -11,20 +11,25 @@ impl SessionId {
     // xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx
     const UPPER_MASK: u128 = 0xFFFFFFFFFFFF << (20 * 4);
     const MIDDIE_BITTIES_MASK: u128 = 0xFFF << (16 * 4);
-    const LOWER_MASK: u128 = 0x2FFFFFFFFFFFFFFF;
+    // Note: leading character should be 3 to mask at 0011 (was 2 -> 0010)
+    // Verified: The more-significant half of the N nibble is used to denote the variant (10xx)
+    const LOWER_MASK: u128 = 0x3FFFFFFFFFFFFFFF;
 
     pub(crate) fn new() -> SessionId {
         // todo doc restriction on upper bits and debug assert
-        SessionId::from_uuid(Uuid::new_v4().as_u128())
+        SessionId::from_uuid(Uuid::new_v4())
     }
 
-    fn from_uuid(uuid: u128) -> SessionId {
-        let upper_masked = uuid & SessionId::UPPER_MASK;
-        let middie_bitties_masked = uuid & SessionId::MIDDIE_BITTIES_MASK;
-        let lower_masked = uuid & SessionId::LOWER_MASK;
+    pub(super) fn from_uuid(uuid: uuid::Uuid) -> SessionId {
+        dbg!(uuid.to_string());
+        let as_u128 = uuid.as_u128();
+        let upper_masked = as_u128 & SessionId::UPPER_MASK;
+        let middie_bitties_masked = as_u128 & SessionId::MIDDIE_BITTIES_MASK;
+        let lower_masked = as_u128 & SessionId::LOWER_MASK;
 
         let upper_masked = upper_masked >> 6;
         let middie_bitties_masked = middie_bitties_masked >> 2;
+
         let id = upper_masked | middie_bitties_masked | lower_masked;
 
         SessionId { id }
@@ -37,5 +42,16 @@ impl SessionId {
     pub(crate) fn stable_from_local_offset(&self, offset_local: LocalId) -> StableId {
         let new_id = self.id + (offset_local.to_generation_count() - 1) as u128;
         StableId::new(new_id)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_uuid_creation() {
+        let uuid = Uuid::new_v4();
+        dbg!(uuid.to_string());
     }
 }
