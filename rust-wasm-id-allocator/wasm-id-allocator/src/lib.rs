51ecdfbd836f2d5a1bd37b5eec06c21c117f7ce0
Cameron Richards <crichards1717@gmail.com>
2023-02-19 12:17:13 -0800

    further scaffolding

--- rust-wasm-id-allocator/wasm-id-allocator/src/lib.rs
+++ rust-wasm-id-allocator/wasm-id-allocator/src/lib.rs
@@ -4,12 +4,3 @@ use wasm_bindgen::prelude::*;
 pub fn add(left: f64, right: f64) -> f64 {
     left + right
 }
-
-#[cfg(test)]
-pub mod test {
-    use crate::add;
-    #[test]
-    pub fn adds_correctly() {
-        assert_eq!(add(1 as f64, 2 as f64), 3 as f64);
-    }
-}
