3133cde9533333c7a8aaca36677183a0ca754cc7
Cameron Richards <crichards1717@gmail.com>
2023-02-19 11:57:17 -0800

    typescript and wasm scaffolding

---
+++ rust-wasm-id-allocator/wasm-id-allocator/src/lib.rs
@@ -0,0 +1,15 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn add(left: f64, right: f64) -> f64 {
+    left + right
+}
+
+#[cfg(test)]
+pub mod test {
+    use crate::add;
+    #[test]
+    pub fn adds_correctly() {
+        assert_eq!(add(1 as f64, 2 as f64), 3 as f64);
+    }
+}
