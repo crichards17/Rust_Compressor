c863c62b4498ebabd41615ac57941309ae3c4de9
taylorsw <mxactionscript@gmail.com>
2023-03-03 10:23:46 -0800

    import compressor types from FF, stub wrapper class

---
+++ typescript-id-allocator/types/index.ts
@@ -0,0 +1,33 @@
+/*!
+ * Copyright (c) Microsoft Corporation and contributors. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export {
+	IdCreationRange,
+	SerializedCluster,
+	SerializedClusterOverrides,
+	SerializedIdCompressor,
+	SerializedIdCompressorWithNoSession,
+	SerializedIdCompressorWithOngoingSession,
+	SerializedLocalOverrides,
+	SerializedLocalState,
+	SerializedSessionData,
+	SerializedSessionIdNormalizer,
+	UnackedLocalId,
+	VersionedSerializedIdCompressor,
+} from "./persisted-types";
+
+export { IIdCompressorCore, IIdCompressor } from "./idCompressor";
+
+export {
+	SessionSpaceCompressedId,
+	OpSpaceCompressedId,
+	SessionId,
+	FinalCompressedId,
+	StableId,
+	UuidString,
+	CompressedId,
+	SessionUnique,
+	LocalCompressedId,
+} from "./identifiers";
