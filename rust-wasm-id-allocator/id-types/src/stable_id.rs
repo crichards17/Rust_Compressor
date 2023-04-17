70501c1609d76b2ec942e3b4bf3ef10da4c6a838
taylorsw <mxactionscript@gmail.com>
2023-04-17 16:32:59 -0700

    uuid tests

--- rust-wasm-id-allocator/id-types/src/stable_id.rs
+++ rust-wasm-id-allocator/id-types/src/stable_id.rs
@@ -102,6 +102,7 @@ impl std::ops::Add<u64> for StableId {
 impl std::ops::Sub<u64> for StableId {
     type Output = Self;
     fn sub(self, rhs: u64) -> Self::Output {
+        debug_assert!(self.id >= rhs as u128);
         StableId {
             id: self.id - rhs as u128,
         }
@@ -118,12 +119,89 @@ impl std::ops::Sub<StableId> for StableId {
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
+
     use super::*;
 
     #[test]
+    fn test_addition() {
+        let stable_id = StableId::from(0x00000000000040008000000000000000);
+        assert_eq!(
+            u128::from(stable_id + 5),
+            0x00000000000040008000000000000005
+        );
+    }
+
+    #[test]
+    fn test_u64_subtraction() {
+        let uuid_1 = StableId::from(0x100000000000400080000000000000F0);
+        assert_eq!(
+            uuid_1 - 3_u64,
+            StableId::from(0x100000000000400080000000000000ED)
+        );
+    }
+
+    #[test]
+    fn test_u128_subtraction() {
+        let uuid_1 = StableId::from(0x10000000000040008000000000000005);
+        let uuid_2 = StableId::from(0x20000000000040008000000000000009);
+        // 0x10000000000040008000000000000009, but remove the four bits for the '4' and the two bits
+        // for the variant '8', and slide the '1' to the right those 6 bits.
+        assert_eq!(uuid_2 - uuid_1, 0x00400000000000000000000000000004);
+    }
+
+    #[test]
+    fn test_math_multiple_uuids() {
+        let mut uuids = vec![
+            Uuid::parse_str("00000000-0000-4000-8000-000000000000").unwrap(),
+            Uuid::parse_str("748540ca-b7c5-4c99-83ff-c1b8e02c09d6").unwrap(),
+            Uuid::parse_str("748540ca-b7c5-4c99-83ef-c1b8e02c09d6").unwrap(),
+            Uuid::parse_str("748540ca-b7c5-4c99-831f-c1b8e02c09d6").unwrap(),
+            Uuid::parse_str("0002c79e-b536-4776-b000-000266c252d5").unwrap(),
+            Uuid::parse_str("082533b9-6d05-4068-a008-fe2cc43543f7").unwrap(),
+            Uuid::parse_str("2c9fa1f8-48d5-4554-a466-000000000000").unwrap(),
+            Uuid::parse_str("2c9fa1f8-48d5-4000-a000-000000000000").unwrap(),
+            Uuid::parse_str("10000000-0000-4000-b000-000000000000").unwrap(),
+            Uuid::parse_str("10000000-0000-4000-b020-000000000000").unwrap(),
+            Uuid::parse_str("10000000-0000-4000-b00f-ffffffffffff").unwrap(),
+            Uuid::parse_str("10000000-0000-4000-b040-000000000000").unwrap(),
+            Uuid::parse_str("f0000000-0000-4000-8000-000000000000").unwrap(),
+            Uuid::parse_str("efffffff-ffff-4fff-bfff-ffffffffffff").unwrap(),
+        ];
+        uuids.sort_by(|uuid_a, uuid_b| StableId::from(*uuid_b).cmp(&StableId::from(*uuid_a)));
+
+        for i in 0..uuids.len() {
+            let uuid_a = uuids[i];
+            let stable_id_a = StableId::from(uuid_a);
+            assert_eq!(stable_id_a - stable_id_a, 0);
+            for num in vec![1, 100, 1000000, 1000000000000000000_u64] {
+                let uuid = Uuid::from(stable_id_a + num);
+                assert_eq!(uuid.get_version_num(), 4);
+                assert_eq!(uuid.get_variant(), uuid::Variant::RFC4122);
+            }
+            for j in i..uuids.len() {
+                let uuid_b = uuids[j];
+                let stable_id_b = StableId::from(uuid_b);
+                let uuid_plus_delta = Uuid::from(StableId {
+                    id: stable_id_b.id + (stable_id_a - stable_id_b),
+                });
+                assert_eq!(uuid_plus_delta, uuid_a);
+            }
+        }
+    }
+
+    #[test]
+    fn test_roundtrip() {
+        let stable_id = StableId::from(0x00000000000040008000000000000000);
+        assert_eq!(
+            StableId::from(Uuid::from_str(&String::from(stable_id)).unwrap()),
+            stable_id
+        );
+    }
+
+    #[test]
     fn test_uuid_increment_spillover() {
-        let uuid = Uuid::from_u128(0xe507602db1504fccBfffffffffffffff);
-        let mut stable_id = StableId::from(uuid);
+        let mut stable_id = StableId::from(0xe507602db1504fccBfffffffffffffff);
         assert_eq!(u128::from(stable_id), 0xe507602db1504fccBfffffffffffffff);
         stable_id = stable_id + 1;
         let uuid = Uuid::from(stable_id);
