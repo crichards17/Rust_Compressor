ce42ddf7d3cda17ed906da46695353f9d36c907b
Cameron Richards <crichards1717@gmail.com>
2023-04-20 13:37:24 -0700

    Moved helpers to test-utils

--- rust-wasm-id-allocator/distributed-id-allocator/tests/compressor_test_utils/mod.rs
+++ rust-wasm-id-allocator/distributed-id-allocator/tests/compressor_test_utils/mod.rs
@@ -1,4 +1,5 @@
 use distributed_id_allocator::compressor::IdCompressor;
+use id_types::SessionSpaceId;
 
 pub fn serialize_roundtrip(compressor: &IdCompressor) -> RoundtrippedCompressors {
     let mut deserialized_vec: Vec<IdCompressor> = Vec::new();
@@ -18,4 +19,27 @@ pub fn serialize_roundtrip(compressor: &IdCompressor) -> RoundtrippedCompressors
 pub struct RoundtrippedCompressors {
     pub with_local: IdCompressor,
     pub without_local: IdCompressor,
+}
+
+pub trait TestSessionSpaceId {
+    fn unwrap_uuid_str(&self, compressor: &IdCompressor) -> String;
+}
+
+impl TestSessionSpaceId for SessionSpaceId {
+    fn unwrap_uuid_str(&self, compressor: &IdCompressor) -> String {
+        compressor.decompress(*self).unwrap().into()
+    }
+}
+
+pub fn generate_n_ids(compressor: &mut IdCompressor, num_ids: i32) -> Vec<SessionSpaceId> {
+    let mut ids = Vec::new();
+    for _ in 0..num_ids {
+        ids.push(compressor.generate_next_id())
+    }
+    ids
+}
+
+pub fn finalize_next_range(compressor: &mut IdCompressor) {
+    let range = compressor.take_next_range();
+    _ = compressor.finalize_range(&range);
 }
