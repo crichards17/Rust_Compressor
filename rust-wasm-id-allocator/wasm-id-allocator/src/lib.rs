cd2198ebedb7de7320a931e387006871c5ac1fb8
taylorsw <mxactionscript@gmail.com>
2023-05-01 14:39:40 -0700

    add allocators

--- rust-wasm-id-allocator/wasm-id-allocator/src/lib.rs
+++ rust-wasm-id-allocator/wasm-id-allocator/src/lib.rs
@@ -44,7 +44,11 @@ use wasm_bindgen::prelude::*;
 
 #[cfg(debug_assertions)]
 #[cfg(target_arch = "wasm32")]
-use std::alloc::System;
+use lol_alloc::{AssumeSingleThreaded, FreeListAllocator};
+
+#[cfg(debug_assertions)]
+//#[cfg(target_arch = "wasm32")]
+// use std::alloc::System;
 #[cfg(debug_assertions)]
 #[cfg(target_arch = "wasm32")]
 use wasm_tracing_allocator::WasmTracingAllocator;
@@ -52,7 +56,8 @@ use wasm_tracing_allocator::WasmTracingAllocator;
 #[cfg(debug_assertions)]
 #[cfg(target_arch = "wasm32")]
 #[global_allocator]
-static GLOBAL_ALLOCATOR: WasmTracingAllocator<System> = WasmTracingAllocator(System);
+static GLOBAL_ALLOCATOR: WasmTracingAllocator<AssumeSingleThreaded<FreeListAllocator>> =
+    unsafe { WasmTracingAllocator(AssumeSingleThreaded::new(FreeListAllocator::new())) };
 
 #[wasm_bindgen]
 #[derive(Debug)]
--- typescript-id-allocator/test/idCompressor.memory.spec.ts
+++ typescript-id-allocator/test/idCompressor.memory.spec.ts
@@ -3,7 +3,7 @@ import { IdCompressor } from "../src/IdCompressor";
 import { SessionId } from "../src/types";
 import { createSessionId } from "../src/utilities";
 
-describe.only("IdCompressor memory", () => {
+describe.skip("IdCompressor memory", () => {
 	it("Trace allocations", async () => {
 		const numSessions = 5;
 		const capacity = 10;
